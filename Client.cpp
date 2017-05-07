b2e5bd8659f00c43d4531831d576104126e98b14
Martinzm1 <zamoramartin1@gmail.com>
2017-05-07 15:52:19 -0500

    Update Client.cpp
    added comments and function descriptions

--- Client.cpp
+++ Client.cpp
@@ -1,5 +1,14 @@
+/*Clint Wetzel
 //Martin Zamora Martinez
-//1000995810
+/CSE 4320 - 001  Embedded Systems II 
+//University of Texas at Arlington
+Real-Time Data Acquasition  with Communication between Server and Client
+
+*****Windows thread and socket implementation adapted from Tx.cpp and Rx.cpp provided by Dr.Walker.
+*****DT9816 used for data acquasition. Some methods to interface with the ST9816 from Data Translations example files.
+*/
+
+/* Includes */
 #include <string.h>
 #include <stdio.h>
 #include <Windows.h>
@@ -12,6 +21,7 @@
 #include <string>
 #include <stdlib.h>
 
+/*structure used for communication*/
 typedef struct sp_comm {
 	WSADATA wsaData;
 	SOCKET cmdrecvsock;
@@ -19,7 +29,7 @@ typedef struct sp_comm {
 	SOCKET datasock;
 	struct sockaddr_in server;
 } *sp_comm_t;
-
+/*structure used to hold flags*/
 typedef struct sp_flags {
 	unsigned int start_system : 1;
 	unsigned int pause_system : 1;
@@ -28,60 +38,54 @@ typedef struct sp_flags {
 	unsigned int restart : 1;
 	unsigned int transmit_data : 1;
 } *sp_flags_t;
-
+/*structure that holds a flag structure and communication structure*/
 typedef struct sp_struct {
 	struct sp_comm		comm;
 	struct sp_flags		flags;
 } *sp_struct_t;
 
-
-
-
+/*Defines*/
 #define ERR_CODE_NONE	0	/* no error */
-#define ERR_CODE_SWI	1	/* software error */
-
-/////// 
+#define ERR_CODE_SWI	1	/* software error */ 
 #define CMD_LENGTH		5
-
 #define ARG_NONE		1
 #define ARG_NUMBER		2
 #define noop ((void)0)
 #define BUF_LEN			1000
 #define B_COEF			101
 #define PI 3.1415926535897
-
+/*sturcture used for DT9816*/
 typedef struct {
 	char cmd[CMD_LENGTH];
 	int arg;
 } cmd_struct_t;
+/*global variables*/
 WSADATA wsaData;
 int x = 1;
-
 /* Thread to interface with the ProfileClient */
 HANDLE hClientThread;
 DWORD dwClientThreadID;
-VOID client_iface_thread(LPVOID parameters);
+VOID client_iface_thread(LPVOID parameters); //function for thread to handle receiving messages
 char snum[5];
-char fName[20];
-char filName[20];
-char sampRate[20];
-char maxFile[40];
-char areaFile[40];
-char minFile[40];
-double buffer[BUF_LEN];
-int bufBusy = 2;
-int lastSample = 0;
+char fName[20];//holds user input for name prefix
+char filName[20];//holds user input for filter file
+char sampRate[20];//hols user input for sampling rate
+char maxFile[40];//appends name prefix with full file name
+char areaFile[40];//appends name prefix with full file name
+char minFile[40];//appends name prefix with full file name
+double buffer[BUF_LEN]; //buffer to hold receiving data
+int bufBusy = 2; //state for buffer filling
+int lastSample = 0; //state to end program
 int dCount;
-double max, min, area;
-void getInputs();
-void processData();
-double maxSignal(double *d_signal, int length);
-double minSignal(double *d_signal, int length);
-double integrate();
+double max, min, area; 
+void getInputs();// function to accept user inputs
+void processData();// function to calculate and process data buffer
+double maxSignal(double *d_signal, int length); //function to return max value in bffer
+double minSignal(double *d_signal, int length);//function to return min value in buffer
+double integrate();//function to calculate area under curve
 
-
-int main()
-{
+				   /*MAIN*/
+int main(){
 	struct sp_struct profiler;
 	struct sockaddr_in saddr;
 	struct hostent *hp;
@@ -101,7 +105,6 @@ int main()
 	/**********************************************************************************
 	* Setup data transmition socket to broadcast data
 	**********************************************************************************/
-
 	hp = (struct hostent*)malloc(sizeof(struct hostent));
 	hp->h_name = (char*)malloc(sizeof(char) * 17);
 	hp->h_addr_list = (char**)malloc(sizeof(char*) * 2);
@@ -111,12 +114,8 @@ int main()
 	hp->h_length = 4;
 
 	//broadcast in 255.255.255.255 network 	
-
 	hp->h_addr_list[0][0] = (signed char)255;//192;129
 	hp->h_addr_list[0][1] = (signed char)255; //168;107
-
-
-
 	hp->h_addr_list[0][2] = (signed char)255; //0;255  
 	hp->h_addr_list[0][3] = (signed char)255; //140;255
 	hp->h_addr_list[0][4] = 0;
@@ -134,13 +133,11 @@ int main()
 		WSACleanup();
 		return(ERR_CODE_NONE);
 	}
-
 	if (connect(comm->datasock, (struct sockaddr*)&saddr, sizeof(saddr)) == SOCKET_ERROR) {
 		fprintf(stderr, "connect(datasock) failed: %d\n", WSAGetLastError());
 		WSACleanup();
 		return(ERR_CODE_SWI);
 	}
-
 	/**********************************************************************************
 	* Setup and bind a socket to listen for commands from client
 	**********************************************************************************/
@@ -159,49 +156,39 @@ int main()
 		WSACleanup();
 		return(ERR_CODE_NONE);
 	}
-
+	/*create thread for receiving messages*/
 	hClientThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)client_iface_thread, (LPVOID)&profiler, 0, &dwClientThreadID);
 	SetThreadPriority(hClientThread, THREAD_PRIORITY_LOWEST);
-
-
 	getInputs();//grab inputs
-				//send data to server
-				//receive data 
 	FILE *fp2;
-	fp2 = fopen(filName, "r");
-	for (int i = 0; i < B_COEF - 1; i++) {
+	fp2 = fopen(filName, "r");// open filter file
+	for (int i = 0; i < B_COEF - 1; i++) {// load filter into filter buffer
 		fscanf(fp2, "%lf\n", &filter[i]);
 	}
-	fclose(fp2);
-	//printf("%s    %s \n", sampRate, fName);
-	send(comm->datasock, fName, sizeof(fName), 0);
-	send(comm->datasock, sampRate, sizeof(sampRate), 0);
-	for (int i = 0; i < B_COEF - 1; i++) {
+	fclose(fp2);//close filter file
+	send(comm->datasock, fName, sizeof(fName), 0); //send file name prefix to server
+	send(comm->datasock, sampRate, sizeof(sampRate), 0); //send sampling rate to server
+	for (int i = 0; i < B_COEF - 1; i++) { //send the values in filter buffer to server
 		sprintf(ParamBuffer, "%f", filter[i]);
 		send(comm->datasock, ParamBuffer, sizeof(ParamBuffer), 0);
 	}
 	while (!lastSample) {
-		if (bufBusy==0) { //buffer complete
+		if (bufBusy == 0) { //buffer complete
 			processData();// process full buffer
 			printf("data processed\n");
 			bufBusy = 3;// dont keep processing same data
 		}
 		if (bufBusy == 1) {//buffer is being filled
-			//printf("waiting for buffer to fill\n");
+						   //printf("waiting for buffer to fill\n");
 			noop;// keep waiting for full buffer
 		}
 		if (bufBusy == 2) {// no buffer recieved yet
-			//printf("no buffer yet\n");
+						   //printf("no buffer yet\n");
 			noop;
 		}
 	}
-	//save file
-	//end
-
-	//processData();
-	printf("Data saved in %s\n",fName);
+	printf("Data saved in %s\n", fName);
 	printf("Ending Client.\n");
-
 	return 0;
 }
 
@@ -209,8 +196,8 @@ int main()
 * VOID client_iface_thread(LPVOID)
 *
 * Description: Thread communicating commands from client and the status of their
-*				completion back to client.
-*
+*				completion back to client. It handles special messages from the Server
+*				to control the states of this program or fill the data buffer.
 *
 ************************************************************************************/
 VOID client_iface_thread(LPVOID parameters) //LPVOID parameters)
@@ -226,26 +213,30 @@ VOID client_iface_thread(LPVOID parameters) //LPVOID parameters)
 	{
 		memset(ParamBuffer, 0, sizeof(ParamBuffer));
 		saddr_len = sizeof(saddr);
-		retval = recvfrom(comm->cmdrecvsock, ParamBuffer, sizeof(ParamBuffer), 0, (struct sockaddr *)&saddr, &saddr_len);
-		if (bufBusy == 1) {
-			buffer[dCount] = atof(ParamBuffer);
+		retval = recvfrom(comm->cmdrecvsock, ParamBuffer, sizeof(ParamBuffer), 0, (struct sockaddr *)&saddr, &saddr_len); //grab input from socket
+		if (bufBusy == 1) { //if filling buffer is activated
+			buffer[dCount] = atof(ParamBuffer);	//copy recieved data to buffer
 			dCount++;
-			//printf("The value is : %s \n", ParamBuffer);
 		}
-		if (strcmp(ParamBuffer, "startBuf") == 0) {
-			bufBusy = 1;
+		if (strcmp(ParamBuffer, "startBuf") == 0) { //when starting buffer message recevied 
+			bufBusy = 1;							//change buffer state to busy
 			dCount = 0;
 		}
-		if (strcmp(ParamBuffer, "endBuf") == 0) {
-			bufBusy = 0;
+		if (strcmp(ParamBuffer, "endBuf") == 0) { //when ending buffer message received
+			bufBusy = 0;						  //change buffer state to not busy
 		}
-		if (strcmp(ParamBuffer, "FINAL") == 0) {
-			lastSample = 1;
+		if (strcmp(ParamBuffer, "FINAL") == 0) {// when ending program message recieved
+			lastSample = 1;						//set state to last sample
 			printf("final message recieved\n");
 		}
 	}
 }
+/*getInput:
+	Description: Void function that prompts the user for a filter name, file name prefix
+				and sampling rate. The filter file must be in Client.cpp directory.
+				This data will then be passed to the Server by socket communication.
 
+*/
 void getInputs() {
 	printf("Input filter filename:\n");
 	scanf("%s", filName);
@@ -261,28 +252,45 @@ void getInputs() {
 	getchar();//wait for enter.
 	return;
 }
+/*processData:
+		Description: processData is a void function that uses a globally stored value buffer
+					and other functions to find the max, min and area under the curve of the
+					signal stored in the value buffer. The data is then also stored in 3 files.
+
+		NOTE:		processData appends files it writes to. If there is an existing file with the 
+					same name, it will only be appeneded. IF there is no such file, the file will 
+					be created.
+*/
 void processData() {
-	FILE *fpMax, *fpArea, *fpMin;
-	fpMax = fopen(maxFile, "a");
+	FILE *fpMax, *fpArea, *fpMin; //3 file pointers
+	//each file opened withe the name stored earlier
+	fpMax = fopen(maxFile, "a"); 
 	fpArea = fopen(areaFile, "a");
 	fpMin = fopen(minFile, "a");
-	max=maxSignal(buffer, BUF_LEN);
-	min = minSignal(buffer, BUF_LEN);
-	area = integrate();
-	printf("area: %f min: %f max: %f\n",area, min, max);
-	fprintf(fpMax,"%f\n", max);
+	
+	max = maxSignal(buffer, BUF_LEN);	//max function called
+	min = minSignal(buffer, BUF_LEN);	//min function called
+	area = integrate();					//area function called
+	printf("area: %f min: %f max: %f\n", area, min, max);	//print data to console
+	//each data stored in its corresponding file
+	fprintf(fpMax, "%f\n", max);		
 	fprintf(fpArea, "%f\n", area);
 	fprintf(fpMin, "%f\n", min);
+	//each file closed
 	fclose(fpMin);
 	fclose(fpArea);
 	fclose(fpMax);
 	return;
-}
+}\
+/*maxSignal function
+	Description: accepts a pointer to a double array and an int of the length of that array to 
+				find the max value in the array.
+*/
 double maxSignal(double *d_signal, int length) {
 	int i;
 	double max = d_signal[0];
 
-	for (i = 100; i < length-100; i++) {
+	for (i = 100; i < length - 100; i++) {// start at 100 to ignore the transient region
 		if (d_signal[i] > max) {
 			max = d_signal[i];
 		}
@@ -290,11 +298,15 @@ double maxSignal(double *d_signal, int length) {
 
 	return max;
 }
+/*minSignal function
+Description: accepts a pointer to a double array and an int of the length of that array to
+find the min value in the array.
+*/
 double minSignal(double *d_signal, int length) {
 	int i;
 	double min = d_signal[100];
 
-	for (i = 100; i < length-100; i++) {
+	for (i = 100; i < length - 100; i++) {// start at 100 to ignore the transient region
 		if (d_signal[i] < min) {
 			min = d_signal[i];
 		}
@@ -302,19 +314,22 @@ double minSignal(double *d_signal, int length) {
 	return min;
 }
 
-
+/* Intergrate void function
+	Description: Integrate is a void function that uses a global value
+				buffer that contains a signal. It uses the trapezoidal function
+				to calculate the area under the curve.
+*/
 double integrate() {
 	int count, max;
 	double sum, lowLim, highLim;
 	lowLim = 0;
-	highLim = PI/2;
-	max = BUF_LEN/4;
+	highLim = PI / 2;
+	max = BUF_LEN / 4;
 	sum = buffer[249];
-	//printf("%f %f\n", buffer[101],buffer[550]);
-	for (int count = 250; count < max-1; count++) {
+	for (int count = 250; count < max - 1; count++) {
 		sum = sum + (2 * buffer[count]);
 	}
-	sum = sum + buffer[max+249];
-	sum = sum*(highLim - lowLim) / (2*max);
+	sum = sum + buffer[max + 249];
+	sum = sum*(highLim - lowLim) / (2 * max);
 	return sum;
 }
